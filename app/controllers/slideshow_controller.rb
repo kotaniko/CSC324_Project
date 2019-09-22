b39f2dbea04ace63c6a535c7e3775a49056de4ab
kotaniko <kotaniko@grinnell.edu>
2019-09-21 20:40:38 -0500

    HW4

--- app/controllers/slideshow_controller.rb
+++ app/controllers/slideshow_controller.rb
@@ -1,4 +1,7 @@
 class SlideshowController < ApplicationController
   def show
+    pic_array = Dir.glob("*.{jpg,JPG}", base: "app/assets/images")
+    rand_number = rand(pic_array.count)
+    @rand_pic = pic_array[rand_number]
   end
 end
