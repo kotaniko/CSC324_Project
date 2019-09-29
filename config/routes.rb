0d7af4285131bd09e8e878b375c01140cdc009aa
kotaniko <kotaniko@grinnell.edu>
2019-09-28 21:07:14 -0500

    HW6 with Failure test case (Red)

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   #get 'slideshow/show'
   get "/show", to: "slideshow#show"
-  get '/slideshow/show'
 
   #get 'album/album'
   get "/album", to: "album#album"
-  get '/album/album'
 
-  get 'homepage/home'
-  #get "/", to: "homepage#home", as: "root"
+  #get 'homepage/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#home'
--- test/controllers/slideshow_controller_test.rb
+++ test/controllers/slideshow_controller_test.rb
@@ -5,6 +5,28 @@ class SlideshowControllerTest < ActionDispatch::IntegrationTest
     get show_path
     assert_response :success
     assert_select "title", "Slideshow | Gallery"
+
+    i = 0
+    pic_name_array = Dir.glob("*.{jpg,JPG}", base: "app/assets/images")
+    iteration_count = pic_name_array.length * 100
+
+    loop do
+      i += 1
+      if i >= iteration_count
+        assert(false, "Slideshow does not show all the pictures.")
+        break
+      end
+
+      get show_path
+      if pic_name_array.include? assigns(:rand_pic)
+        pic_name_array.delete assigns(:rand_pic)
+      end
+
+      if pic_name_array.empty?
+        assert true
+        break
+      end
+    end
   end
 
 end
