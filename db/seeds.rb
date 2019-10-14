730569a14fa17f9a91cad660c83afff282deb53c
kotaniko <kotaniko@grinnell.edu>
2019-10-13 20:13:04 -0500

    HW7_Database_Fixed
    'rails test' produced error about data format in an array column. Fixed the data format in test/fixture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Collection.create(name: "Yakushima", picture: ["IMG_4714.JPG", "IMG_4719.JPG", "IMG_4736.jpg", "IMG_4739.jpg", "IMG_4754.jpg", "IMG_4769.jpg", "IMG_4887.JPG", "IMG_4892.jpg", "IMG_4901.jpg", "IMG_4904.jpg", "IMG_4922.jpg", "IMG_4939.jpg", "IMG_4947.jpg", "IMG_4958.jpg", "IMG_4963.JPG", "IMG_4975.jpg", "IMG_4993.jpg"])
-Collection.create(name: "Mt. Fuji", picture: ["2018_08_10_9.jpg"])
-Collection.create(name: "Grand Circle", picture: ["DSC_0183.JPG"])
-Collection.create(name: "Southern Alps Winter", picture: ["IMG_5846.JPG"])
-Collection.create(name: "Yellowstone/Grand Teton", picture: ["IMG_6136.JPG", "IMG_6340.JPG"])
+Collection.create(name: 'Yakushima', picture: '{"IMG_4714.JPG","IMG_4719.JPG","IMG_4736.jpg","IMG_4739.jpg","IMG_4754.jpg","IMG_4769.jpg","IMG_4887.JPG","IMG_4892.jpg","IMG_4901.jpg","IMG_4904.jpg","IMG_4922.jpg","IMG_4939.jpg","IMG_4947.jpg","IMG_4958.jpg","IMG_4963.JPG","IMG_4975.jpg","IMG_4993.jpg"}')
+Collection.create(name: 'Mt. Fuji', picture: '{"2018_08_10_9.jpg"}')
+Collection.create(name: 'Grand Circle', picture: '{"DSC_0183.JPG"}')
+Collection.create(name: 'Southern Alps Winter', picture: '{"IMG_5846.JPG"}')
+Collection.create(name: 'Yellowstone/Grand Teton', picture: '{"IMG_6136.JPG","IMG_6340.JPG"}')
